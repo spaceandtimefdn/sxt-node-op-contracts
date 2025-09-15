40a44e8d657c2b6b7fc65f474d2c73a57f765909
Jay White <JayWhite2357@users.noreply.github.com>
2025-09-15 10:07:36 -0400

    feat: add `fundedMessage` to `SXTChainMessaging`

--- src/interfaces/ISXTChainMessaging.sol
+++ src/interfaces/ISXTChainMessaging.sol
@@ -10,9 +10,23 @@ interface ISXTChainMessaging {
     /// @param nonce The nonce of the message
     event Message(address sender, bytes body, uint248 nonce);
 
+    /// @notice Emitted when a funded message is sent to the SXT Chain
+    /// @param sender The address of the sender
+    /// @param body The message being sent
+    /// @param nonce The nonce of the message
+    /// @param target The address receiving the tokens
+    /// @param amount The amount of tokens transferred
+    event FundedMessage(address sender, bytes body, uint248 nonce, address target, uint248 amount);
+
     /// @notice Send a message to the SXT Chain
     /// @param body The message to send
     function message(bytes calldata body) external;
+
+    /// @notice Send a funded message to the SXT Chain with ERC20 transfer
+    /// @param body The message to send
+    /// @param target The address to transfer tokens to
+    /// @param amount The amount of tokens to transfer
+    function fundedMessage(bytes calldata body, address target, uint248 amount) external;
 
     /// @notice Get the current nonce for a sender
     /// @param sender The address to get the nonce for
--- test/SXTChainMessagingTest.t.sol
+++ test/SXTChainMessagingTest.t.sol
@@ -3,14 +3,18 @@ pragma solidity 0.8.28;
 
 import {Test} from "forge-std/Test.sol";
 import {SXTChainMessaging} from "../src/SXTChainMessaging.sol";
+import {MockERC20} from "./mocks/MockERC20.sol";
 
 contract SXTChainMessagingTest is Test {
     SXTChainMessaging private messaging;
+    MockERC20 private token;
 
     event Message(address sender, bytes body, uint248 nonce);
+    event FundedMessage(address sender, bytes body, uint248 nonce, address target, uint248 amount);
 
     function setUp() public {
-        messaging = new SXTChainMessaging();
+        token = new MockERC20();
+        messaging = new SXTChainMessaging(address(token));
     }
 
     function testMessageNonceIncrement() public {
@@ -107,5 +111,144 @@ contract SXTChainMessagingTest is Test {
         messaging.message("test2");
         assertEq(messaging.getNonce(sender1), 1);
         assertEq(messaging.getNonce(sender2), 1);
+    }
+
+    function testFundedMessageEmitsCorrectEvent() public {
+        address target = address(0x123);
+        uint248 amount = 100;
+        bytes memory testMessage = "funded test";
+
+        token.mint(address(this), amount);
+        token.approve(address(messaging), amount);
+
+        vm.expectEmit(true, true, true, true);
+        emit FundedMessage(address(this), testMessage, 1, target, amount);
+
+        messaging.fundedMessage(testMessage, target, amount);
+    }
+
+    function testFundedMessageTransfersTokens() public {
+        address target = address(0x123);
+        uint248 amount = 100;
+
+        token.mint(address(this), amount);
+        token.approve(address(messaging), amount);
+
+        uint256 initialSenderBalance = token.balanceOf(address(this));
+        uint256 initialTargetBalance = token.balanceOf(target);
+
+        messaging.fundedMessage("test", target, amount);
+
+        assertEq(token.balanceOf(address(this)), initialSenderBalance - amount);
+        assertEq(token.balanceOf(target), initialTargetBalance + amount);
+    }
+
+    function testFundedMessageIncrementsNonce() public {
+        address target = address(0x123);
+        uint248 amount = 100;
+
+        token.mint(address(this), amount * 2);
+        token.approve(address(messaging), amount * 2);
+
+        assertEq(messaging.getNonce(address(this)), 0);
+
+        messaging.fundedMessage("first", target, amount);
+        assertEq(messaging.getNonce(address(this)), 1);
+
+        messaging.fundedMessage("second", target, amount);
+        assertEq(messaging.getNonce(address(this)), 2);
+    }
+
+    function testFundedMessageNoncePerSender() public {
+        address sender1 = address(0x1);
+        address sender2 = address(0x2);
+        address target = address(0x123);
+        uint248 amount = 100;
+
+        token.mint(sender1, amount * 2);
+        token.mint(sender2, amount * 2);
+
+        vm.startPrank(sender1);
+        token.approve(address(messaging), amount * 2);
+        vm.expectEmit(true, true, true, true);
+        emit FundedMessage(sender1, "first", 1, target, amount);
+        messaging.fundedMessage("first", target, amount);
+        vm.stopPrank();
+
+        vm.startPrank(sender2);
+        token.approve(address(messaging), amount * 2);
+        vm.expectEmit(true, true, true, true);
+        emit FundedMessage(sender2, "first", 1, target, amount);
+        messaging.fundedMessage("first", target, amount);
+        vm.stopPrank();
+
+        vm.prank(sender1);
+        vm.expectEmit(true, true, true, true);
+        emit FundedMessage(sender1, "second", 2, target, amount);
+        messaging.fundedMessage("second", target, amount);
+
+        assertEq(messaging.getNonce(sender1), 2);
+        assertEq(messaging.getNonce(sender2), 1);
+    }
+
+    function testFundedMessageRevertsInsufficientAllowance() public {
+        address target = address(0x123);
+        uint248 amount = 100;
+
+        token.mint(address(this), amount);
+        token.approve(address(messaging), amount - 1);
+
+        vm.expectRevert();
+        messaging.fundedMessage("test", target, amount);
+    }
+
+    function testFundedMessageRevertsInsufficientBalance() public {
+        address target = address(0x123);
+        uint248 amount = 100;
+
+        token.approve(address(messaging), amount);
+
+        vm.expectRevert();
+        messaging.fundedMessage("test", target, amount);
+    }
+
+    function testFundedMessageWithZeroAmount() public {
+        address target = address(0x123);
+        uint248 amount = 0;
+
+        vm.expectEmit(true, true, true, true);
+        emit FundedMessage(address(this), "zero amount", 1, target, amount);
+
+        messaging.fundedMessage("zero amount", target, amount);
+
+        assertEq(messaging.getNonce(address(this)), 1);
+    }
+
+    function testFuzzFundedMessage(address sender, address target, bytes calldata testMessage, uint248 amount) public {
+        vm.assume(sender != address(0) && target != address(0) && sender != target);
+        vm.assume(amount > 0 && amount < type(uint128).max); // Reasonable amount bounds
+
+        token.mint(sender, amount);
+
+        vm.startPrank(sender);
+        token.approve(address(messaging), amount);
+
+        vm.expectEmit(true, true, true, true);
+        emit FundedMessage(sender, testMessage, 1, target, amount);
+
+        messaging.fundedMessage(testMessage, target, amount);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(target), amount);
+        assertEq(messaging.getNonce(sender), 1);
+    }
+
+    function testTokenAddressIsSet() public {
+        assertEq(messaging.TOKEN_ADDRESS(), address(token));
+    }
+
+    function testConstructorRevertsZeroAddress() public {
+        vm.expectRevert(SXTChainMessaging.ZeroAddress.selector);
+        new SXTChainMessaging(address(0));
     }
 }
