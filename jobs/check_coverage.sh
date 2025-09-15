5d482b820cfce80fbbd7f696229d1935ffa90e16
Jay White <JayWhite2357@users.noreply.github.com>
2025-09-15 10:24:20 -0400

    test: add tests to resolve missing converage

--- jobs/check_coverage.sh
+++ jobs/check_coverage.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 JOBS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $JOBS_DIR/..
 
-percentage=$(forge clean > /dev/null && forge build > /dev/null  && forge coverage | grep -o "[0-9\.]*%" | uniq | tr -d '\n')
+percentage=$(forge clean > /dev/null && forge build > /dev/null && forge coverage | grep -o "[0-9\.]*%" | uniq | tr -d '\n')
 if [ "$percentage" != "%100.00%" ]; then
     >&2 echo "missing test coverage!"
     exit 1
